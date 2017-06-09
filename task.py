08628fd6066ca3e44cac182a06e7699d82481fbd
ray <rayner@robots.ox.ac.uk>
2017-06-09 16:20:24 +0100

    added argparse, summary writer

--- task.py
+++ task.py
@@ -1,5 +1,7 @@
-#encoding: utf-8
+"""Run for main task."""
+# encoding: utf-8
 
+import argparse
 from datetime import datetime
 from tensorflow.python.platform import gfile
 import numpy as np
@@ -7,44 +9,41 @@ import tensorflow as tf
 from dataset import DataSet
 from dataset import output_predict
 import model
+import sys
 import train_operation as op
 
-MAX_STEPS = 10000000
-LOG_DEVICE_PLACEMENT = False
-BATCH_SIZE = 8
-TRAIN_FILE = "train.csv"
-COARSE_DIR = "coarse"
-REFINE_DIR = "refine"
-
-REFINE_TRAIN = True
-FINE_TUNE = True
 
 def train():
+    """Train."""
     with tf.Graph().as_default():
         global_step = tf.Variable(0, trainable=False)
-        dataset = DataSet(BATCH_SIZE)
-        images, depths, invalid_depths = dataset.csv_inputs(TRAIN_FILE)
+        dataset = DataSet(FLAGS.batch_size)
+        images, depths, invalid_depths = dataset.csv_inputs(FLAGS.train_file)
         keep_conv = tf.placeholder(tf.float32)
         keep_hidden = tf.placeholder(tf.float32)
-        if REFINE_TRAIN:
+        if FLAGS.refine_train:
             print("refine train.")
             coarse = model.inference(images, keep_conv, trainable=False)
-            logits = model.inference_refine(images, coarse, keep_conv, keep_hidden)
+            logits = model.inference_refine(images, coarse, keep_conv,
+                                            keep_hidden)
         else:
             print("coarse train.")
             logits = model.inference(images, keep_conv, keep_hidden)
         loss = model.loss(logits, depths, invalid_depths)
-        train_op = op.train(loss, global_step, BATCH_SIZE)
+        train_op = op.train(loss, global_step, FLAGS.batch_size)
         init_op = tf.initialize_all_variables()
 
         # Session
-        sess = tf.Session(config=tf.ConfigProto(log_device_placement=LOG_DEVICE_PLACEMENT))
-        sess.run(init_op)    
+        sess = tf.Session(config=tf.ConfigProto(
+                          log_device_placement=FLAGS.log_device_placement))
+        merged = tf.summary.merge_all()
+        summary_writer = tf.summary.FileWriter(FLAGS.log_dir, sess.graph)
+        sess.run(init_op)
 
         # parameters
         coarse_params = {}
         refine_params = {}
-        if REFINE_TRAIN:
+        if FLAGS.refine_train:
             for variable in tf.all_variables():
                 variable_name = variable.name
                 print("parameter: %s" % (variable_name))
@@ -52,13 +51,13 @@ def train():
                     continue
                 if variable_name.find('coarse') >= 0:
                     coarse_params[variable_name] = variable
-                print("parameter: %s" %(variable_name))
+                print("parameter: %s" % (variable_name))
                 if variable_name.find('fine') >= 0:
                     refine_params[variable_name] = variable
         else:
             for variable in tf.trainable_variables():
                 variable_name = variable.name
-                print("parameter: %s" %(variable_name))
+                print("parameter: %s" % (variable_name))
                 if variable_name.find("/") < 0 or variable_name.count("/") != 1:
                     continue
                 if variable_name.find('coarse') >= 0:
@@ -68,22 +67,23 @@ def train():
         # define saver
         print coarse_params
         saver_coarse = tf.train.Saver(coarse_params)
-        if REFINE_TRAIN:
+        if FLAGS.refine_train:
             saver_refine = tf.train.Saver(refine_params)
         # fine tune
-        if FINE_TUNE:
-            coarse_ckpt = tf.train.get_checkpoint_state(COARSE_DIR)
+        if FLAGS.fine_tune:
+            coarse_ckpt = tf.train.get_checkpoint_state(FLAGS.coarse_dir)
             if coarse_ckpt and coarse_ckpt.model_checkpoint_path:
                 print("Pretrained coarse Model Loading.")
                 saver_coarse.restore(sess, coarse_ckpt.model_checkpoint_path)
                 print("Pretrained coarse Model Restored.")
             else:
                 print("No Pretrained coarse Model.")
-            if REFINE_TRAIN:
-                refine_ckpt = tf.train.get_checkpoint_state(REFINE_DIR)
+            if FLAGS.refine_train:
+                refine_ckpt = tf.train.get_checkpoint_state(FLAGS.refine_dir)
                 if refine_ckpt and refine_ckpt.model_checkpoint_path:
                     print("Pretrained refine Model Loading.")
-                    saver_refine.restore(sess, refine_ckpt.model_checkpoint_path)
+                    saver_refine.restore(sess,
+                                         refine_ckpt.model_checkpoint_path)
                     print("Pretrained refine Model Restored.")
                 else:
                     print("No Pretrained refine Model.")
@@ -91,39 +91,70 @@ def train():
         # train
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-        for step in xrange(MAX_STEPS):
+        for step in xrange(FLAGS.max_steps):
             index = 0
             for i in xrange(1000):
-                _, loss_value, logits_val, images_val = sess.run([train_op, loss, logits, images], feed_dict={keep_conv: 0.8, keep_hidden: 0.5})
+                summary, _, loss_value, logits_val, images_val = sess.run(
+                        [merged, train_op, loss, logits, images],
+                        feed_dict={keep_conv: 0.8, keep_hidden: 0.5})
                 if index % 10 == 0:
-                    print("%s: %d[epoch]: %d[iteration]: train loss %f" % (datetime.now(), step, index, loss_value))
-                    assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+                    print("%s: %d[epoch]: %d[iteration]: train loss %f" % (
+                        datetime.now(), step, index, loss_value))
+                    summary_writer.add_summary(summary, 1000**step+i)
+                    assert not np.isnan(
+                        loss_value), 'Model diverged with loss = NaN'
                 if index % 500 == 0:
-                    if REFINE_TRAIN:
-                        output_predict(logits_val, images_val, "data/predict_refine_%05d_%05d" % (step, i))
+                    if FLAGS.refine_train:
+                        output_predict(logits_val, images_val,
+                                       "data/predict_refine_%05d_%05d" %
+                                       (step, i))
                     else:
-                        output_predict(logits_val, images_val, "data/predict_%05d_%05d" % (step, i))
+                        output_predict(logits_val, images_val,
+                                       "data/predict_%05d_%05d" % (step, i))
                 index += 1
 
-            if step % 5 == 0 or (step * 1) == MAX_STEPS:
-                if REFINE_TRAIN:
-                    refine_checkpoint_path = REFINE_DIR + '/model.ckpt'
-                    saver_refine.save(sess, refine_checkpoint_path, global_step=step)
+            if step % 5 == 0 or (step * 1) == FLAGS.max_steps:
+                if FLAGS.refine_train:
+                    refine_checkpoint_path = FLAGS.refine_dir + '/model.ckpt'
+                    saver_refine.save(sess, refine_checkpoint_path,
+                                      global_step=step)
                 else:
-                    coarse_checkpoint_path = COARSE_DIR + '/model.ckpt'
-                    saver_coarse.save(sess, coarse_checkpoint_path, global_step=step)
+                    coarse_checkpoint_path = FLAGS.coarse_dir + '/model.ckpt'
+                    saver_coarse.save(sess, coarse_checkpoint_path,
+                                      global_step=step)
         coord.request_stop()
         coord.join(threads)
         sess.close()
 
 
 def main(argv=None):
-    if not gfile.Exists(COARSE_DIR):
-        gfile.MakeDirs(COARSE_DIR)
-    if not gfile.Exists(REFINE_DIR):
-        gfile.MakeDirs(REFINE_DIR)
+    """Main."""
+    if not gfile.Exists(FLAGS.coarse_dir):
+        gfile.MakeDirs(FLAGS.coarse_dir)
+    if not gfile.Exists(FLAGS.refine_dir):
+        gfile.MakeDirs(FLAGS.refine_dir)
     train()
 
 
 if __name__ == '__main__':
-    tf.app.run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--max_steps', type=int, default=10000000,
+                        help='Max steps')
+    parser.add_argument('--log_device_placement', action='store_false',
+                        help='Log device placement')
+    parser.add_argument('--batch_size', type=int, default=8,
+                        help='Batch size')
+    parser.add_argument('--train_file', type=str, default='train.csv',
+                        help='Train file')
+    parser.add_argument('--coarse_dir', type=str, default='coarse',
+                        help='Coarse directory')
+    parser.add_argument('--refine_dir', type=str, default='refine',
+                        help='Refine directory')
+    parser.add_argument('--refine_train', action='store_true',
+                        help='Refine train')
+    parser.add_argument('--fine_tune', action='store_true',
+                        help='Fine tune')
+    parser.add_argument('--log_dir', type=str, default='logs',
+                        help='Log directory')
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
