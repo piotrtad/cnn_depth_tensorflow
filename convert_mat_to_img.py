fa8f9d0f9f2e77866f568194faffaca6715fd58c
ray <rayner@robots.ox.ac.uk>
2017-06-09 13:14:20 +0100

    added command line args and directory exists check

--- convert_mat_to_img.py
+++ convert_mat_to_img.py
@@ -1,15 +1,26 @@
-#encoding: utf-8
+"""Convert NYUv2 dataset to PNG images."""
+# encoding: utf-8
+import argparse
 import os
 import numpy as np
 import h5py
 from PIL import Image
 import random
+import sys
+import tensorflow as tf
+
+FLAGS = None
 
 
-def convert_nyu(path):
-    print("load dataset: %s" % (path))
-    f = h5py.File(path)
+def main(unused_argv):
+    """Main."""
+    print("load dataset: %s" % (FLAGS.nyu_path))
+    f = h5py.File(FLAGS.nyu_path)
 
+    data_dir = os.path.join('data', 'nyu_datasets')
+
+    if not tf.gfile.Exists(data_dir):
+        tf.gfile.MakeDirs(data_dir)
 
     trains = []
     for i, (image, depth) in enumerate(zip(f['images'], f['depths'])):
@@ -18,9 +29,9 @@ def convert_nyu(path):
         re_depth = (ra_depth/np.max(ra_depth))*255.0
         image_pil = Image.fromarray(np.uint8(ra_image))
         depth_pil = Image.fromarray(np.uint8(re_depth))
-        image_name = os.path.join("data", "nyu_datasets", "%05d.jpg" % (i))
+        image_name = os.path.join(data_dir, "%05d.jpg" % (i))
         image_pil.save(image_name)
-        depth_name = os.path.join("data", "nyu_datasets", "%05d.png" % (i))
+        depth_name = os.path.join(data_dir, "%05d.png" % (i))
         depth_pil.save(depth_name)
 
         trains.append((image_name, depth_name))
@@ -32,7 +43,11 @@ def convert_nyu(path):
             output.write("%s,%s" % (image_name, depth_name))
             output.write("\n")
 
+
 if __name__ == '__main__':
-    current_directory = os.getcwd()
-    nyu_path = 'data/nyu_depth_v2_labeled.mat'
-    convert_nyu(nyu_path)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--nyu_path', type=str,
+                        default='data/nyu_depth_v2_labeled.mat',
+                        help='Path containing labelled NYUv2 dataset')
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
